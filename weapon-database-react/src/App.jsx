2ad43ddf93ce6919c0cd46b1706f886e3bb6d885
Parth Das <parthivdas72@gmail.com>
2026-04-05 23:02:37 +0530

    fix: clean routing for admin panel

--- weapon-database-react/src/App.jsx
+++ weapon-database-react/src/App.jsx
@@ -1,23 +1,50 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
-import CategoriesPage from "./pages/CategoriesPage";
+import AdminRoute from "./components/AdminRoute";
+
+// Public Pages
 import HomePage from "./pages/HomePage";
-import SearchResultsPage from "./pages/SearchResultsPage";
-import WeaponDetailsPage from "./pages/WeaponDetailsPage";
 import WeaponsPage from "./pages/WeaponsPage";
+import WeaponDetailsPage from "./pages/WeaponDetailsPage";
+import CategoriesPage from "./pages/CategoriesPage";
+import SearchResultsPage from "./pages/SearchResultsPage";
+
+// Admin Pages
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminWeapons from "./pages/AdminWeapons";
+import AddWeapon from "./pages/AddWeapon";
+import EditWeapon from "./pages/EditWeapon";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/weapons" element={<WeaponsPage />} />
-          <Route path="/weapons/:id" element={<WeaponDetailsPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-          <Route path="/search" element={<SearchResultsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
+
+        {/* PUBLIC ROUTES */}
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="weapons" element={<WeaponsPage />} />
+          <Route path="weapon/:id" element={<WeaponDetailsPage />} />
+          <Route path="categories" element={<CategoriesPage />} />
+          <Route path="search" element={<SearchResultsPage />} />
         </Route>
+
+        {/* ADMIN LOGIN */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+
+        {/* PROTECTED ADMIN ROUTES */}
+        <Route element={<AdminRoute />}>
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/weapons" element={<AdminWeapons />} />
+          <Route path="/admin/add" element={<AddWeapon />} />
+          <Route path="/admin/edit/:id" element={<EditWeapon />} />
+        </Route>
+
+        {/* FALLBACK */}
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+
       </Routes>
     </BrowserRouter>
   );
