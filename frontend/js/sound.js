ad7240d9b3283dee966f82a777841d89fb93069b
Parth Das <parthivdas72@gmail.com>
2026-04-07 12:15:35 +0530

    Updated sound.js and added weapon images

--- frontend/js/sound.js
+++ frontend/js/sound.js
@@ -2,19 +2,35 @@ const hoverSound = new Audio("sounds/hover.mp3");
 const clickSound = new Audio("sounds/click.mp3");
 const bgMusic = new Audio("sounds/bg-music.mp3");
 
+bgMusic.preload = "auto";
 bgMusic.loop = true;
-bgMusic.volume = 0.08;
+bgMusic.volume = 0.1;
 
 let musicStarted = false;
 
-document.addEventListener("click", () => {
+function startBackgroundMusic() {
+if (musicStarted) {
+return;
+}
 
-if(!musicStarted){
-bgMusic.play().catch(()=>{});
+const playPromise = bgMusic.play();
+
+if (playPromise && typeof playPromise.then === "function") {
+playPromise
+.then(() => {
+musicStarted = true;
+})
+.catch(() => {
+musicStarted = false;
+});
+return;
+}
+
 musicStarted = true;
 }
 
-});
+document.addEventListener("pointerdown", startBackgroundMusic);
+document.addEventListener("keydown", startBackgroundMusic);
 
 /* hover sound for ALL buttons including dynamic ones */
 
